05083f8ff947ee9b74e285125ee457e980c14121
unreal-zorro <unrealzorro@gmail.com>
2023-03-07 15:59:25 +0300

    todo DTO model is ready

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders text 'Hello Todos!!!'", () => {
   render(<App />);
   const linkElement = screen.getByText(/Hello Todos!!!/i);
   expect(linkElement).toBeInTheDocument();
--- src/components/base/base.component.test.tsx
+++ src/components/base/base.component.test.tsx
@@ -1,26 +1,38 @@
+// import React from "react";
+// import { render, unmountComponentAtNode } from "react-dom";
+// import { act } from "react-dom/test-utils";
+//
+// import { BaseComponent } from "./base.component";
+//
+// let container: Element | null = null;
+// beforeEach(() => {
+//   // подготавливаем DOM-элемент, куда будем рендерить
+//   container = document.createElement("div");
+//   document.body.appendChild(container);
+// });
+//
+// afterEach(() => {
+//   // подчищаем после завершения
+//   if (container) {
+//     unmountComponentAtNode(container);
+//     container.remove();
+//     container = null;
+//   }
+// });
+//
+// it("should contain render Base component", () => {
+//   act(() => {
+//     render(<BaseComponent />, container);
+//   });
+//   expect(container).toBeTruthy();
+// });
+
 import React from "react";
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
+import { render, screen } from "@testing-library/react";
 
 import { BaseComponent } from "./base.component";
 
-let container: Element | null = null;
-beforeEach(() => {
-  // подготавливаем DOM-элемент, куда будем рендерить
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  // подчищаем после завершения
-  container ? unmountComponentAtNode(container) : undefined;
-  container?.remove();
-  container = null;
-});
-
-it("should contain render Base component", () => {
-  act(() => {
-    render(<BaseComponent />, container);
-  });
-  expect(container).toBeTruthy();
+test("renders base component", () => {
+  render(<BaseComponent />);
+  expect(screen).toBeTruthy();
 });
